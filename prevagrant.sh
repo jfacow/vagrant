bc6db8b14773ffdb41fac0b3c05909d818dab64f
Ilya <jfacow@gmail.com>
2018-04-17 12:38:59 +0300

    update prevagrant.sh

--- prevagrant.sh
+++ prevagrant.sh
@@ -1,5 +1,5 @@
 #apt update
 echo "deb https://download.virtualbox.org/virtualbox/debian `lsb_release -c | awk '{print $2}'` contrib" >> /etc/apt/sources.list
-#apt update
-# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys KEY_PLAIN
-# apt install vagrant virtualbox -y
+apt update
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A2F683C52980AECF
+apt install vagrant virtualbox -y
